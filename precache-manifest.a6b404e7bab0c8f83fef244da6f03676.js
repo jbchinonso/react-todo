87be1352f62fafb38712f2ccd13a75b1c495c678
jbchinonso <nwekejohnbosco@gmail.com>
2020-03-03 14:08:14 -0800

    Updates

---
+++ precache-manifest.a6b404e7bab0c8f83fef244da6f03676.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31f24e8a0766a52a66e332ca3e8c66a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90a28b15a5219e99e20a",
+    "url": "/static/css/main.25c6ec2e.chunk.css"
+  },
+  {
+    "revision": "1537900c8cbf9ae30ab8",
+    "url": "/static/js/2.58e2495f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.58e2495f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90a28b15a5219e99e20a",
+    "url": "/static/js/main.4265efac.chunk.js"
+  },
+  {
+    "revision": "452efe56c2036ff11208",
+    "url": "/static/js/runtime-main.83c66be5.js"
+  }
+]);
